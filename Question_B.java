88f1a3a194e30ad588ae14fce8bba93c9f9ca19e
har7rrag <134731465+har7rrag@users.noreply.github.com>
2023-06-28 15:17:54 +0400

    Add files via upload
    the final code with the correct output for the 2nd part of the project

--- Question_B.java
+++ Question_B.java
@@ -1,113 +1,95 @@
 package PROJECT;
 
 public class Question_B {
+
     static class SharedVariables {
-        int A1;
-        int B1;
-        int B2;
-        int A2;
-        int B3;
-        int A3;
-    }
-
-    static class ThreadA extends Thread {
-        private final SharedVariables sharedVariables;
-        private final Object lockObject;
-
-        public ThreadA(SharedVariables sharedVariables, Object lockObject) {
-            this.sharedVariables = sharedVariables;
-            this.lockObject = lockObject;
-        }
-
-        @Override
-        public void run() {
-            synchronized (lockObject) {
-                sharedVariables.A1 = Utility.sumUpToN(100);
-                lockObject.notifyAll();
-                try {
-                    lockObject.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                sharedVariables.A2 = sharedVariables.B2 + Utility.sumUpToN(400);
-                lockObject.notifyAll();
-                try {
-                    lockObject.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                sharedVariables.A3 = sharedVariables.B3 + Utility.sumUpToN(600);
-                lockObject.notifyAll();
-            }
-        }
-    }
-
-    static class ThreadB extends Thread {
-        private final SharedVariables sharedVariables;
-        private final Object lockObject;
-
-        public ThreadB(SharedVariables sharedVariables, Object lockObject) {
-            this.sharedVariables = sharedVariables;
-            this.lockObject = lockObject;
-        }
-
-        @Override
-        public void run() {
-            synchronized (lockObject) {
-                try {
-                    lockObject.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                sharedVariables.B1 = sharedVariables.A1 + Utility.sumUpToN(200);
-                lockObject.notifyAll();
-                try {
-                    lockObject.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                sharedVariables.B2 = Utility.sumUpToN(300);
-                lockObject.notifyAll();
-                try {
-                    lockObject.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                sharedVariables.B3 = sharedVariables.A2 + Utility.sumUpToN(500);
-                lockObject.notifyAll();
-            }
-        }
-    }
-
-    static class Utility {
-        public static int sumUpToN(int n) {
-            return n * (n + 1) / 2;
-        }
+        int a1 = 0;
+        int b1 = 0;
+        int b2 = 0;
+        int a2 = 0;
+        int b3 = 0;
+        int a3 = 0;
     }
 
     public static void main(String[] args) {
         SharedVariables sharedVariables = new SharedVariables();
         Object lockObject = new Object();
 
-        ThreadA threadA = new ThreadA(sharedVariables, lockObject);
-        ThreadB threadB = new ThreadB(sharedVariables, lockObject);
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lockObject) {
+                    // calculate a1
+                    sharedVariables.a1 = sumUpToN(100);
+                    
+                    // calculate b1 using a1
+                    sharedVariables.b1 = sharedVariables.a1 + sumUpToN(200);
+                    lockObject.notify(); // notify thread2 to execute b2
+                    
+                    try {
+                        lockObject.wait(); // wait for thread2 to finish executing b2
+                        
+                        // calculate a2 using b2
+                        sharedVariables.a2 = sharedVariables.b2 + sumUpToN(400);
+                        lockObject.notify(); // notify thread2 to execute b3
+                        
+                        lockObject.wait(); // wait for thread2 to finish executing b3
+                        
+                        // calculate a3 using b3
+                        sharedVariables.a3 = sharedVariables.b3 + sumUpToN(600);
+                        lockObject.notify(); // notify thread2 to finish execution
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
 
-        threadA.start();
-        threadB.start();
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lockObject) {
+                    try {
+                        lockObject.wait(); // wait for thread1 to execute a1
+                        
+                        // calculate b2
+                        sharedVariables.b2 = sumUpToN(300);
+                        lockObject.notify(); // notify thread1 to continue and execute b3
+                        
+                        lockObject.wait(); // wait for thread1 to execute b3
+                        
+                        // calculate b3 using a2
+                        sharedVariables.b3 = sharedVariables.a2 + sumUpToN(500);
+                        lockObject.notify(); // notify thread1 to finish execution
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
 
         try {
-            threadA.join();
-            threadB.join();
+            thread1.join();
+            thread2.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        System.out.println("Final values of shared variables:");
-        System.out.println("A1: " + sharedVariables.A1);
-        System.out.println("B1: " + sharedVariables.B1);
-        System.out.println("B2: " + sharedVariables.B2);
-        System.out.println("A2: " + sharedVariables.A2);
-        System.out.println("B3: " + sharedVariables.B3);
-        System.out.println("A3: " + sharedVariables.A3);
+        // display final values of shared variables
+        System.out.println("final values of shared variables:");
+        System.out.println("a1: " + sharedVariables.a1);
+        System.out.println("b1: " + sharedVariables.b1);
+        System.out.println("b2: " + sharedVariables.b2);
+        System.out.println("a2: " + sharedVariables.a2);
+        System.out.println("b3: " + sharedVariables.b3);
+        System.out.println("a3: " + sharedVariables.a3);
+    }
+
+    // utility method to calculate the sum of numbers up to N
+    public static int sumUpToN(int n) {
+        return n * (n + 1) / 2;
     }
 }
